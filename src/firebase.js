7dc5e970767f624264d939ca8d08d14c3c0d481e
fadiel4721 <fadielintip@gmail.com>
2025-01-07 15:52:59 +0700

    Membuat Project React Tugas

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzY9Moq4wf1du_V9Ewdv6iQmkz_AqN0ps",
+    authDomain: "myreactappfadiel.firebaseapp.com",
+    projectId: "myreactappfadiel",
+    storageBucket: "myreactappfadiel.firebasestorage.app",
+    messagingSenderId: "343440888592",
+    appId: "1:343440888592:web:16fad6d5a79d43b078cb95",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
